92fb192ea415e97e2cdf45a300d13acdbf63c69b
danielavescia <danivescia@gmail.com>
2026-06-17 10:33:54 -0300

    refactor: simplify ProdutoServiceTest by removing unnecessary stages and updating test methods

--- src/test/java/br/com/spock/service/ProdutoServiceTest.java
+++ src/test/java/br/com/spock/service/ProdutoServiceTest.java
@@ -1,62 +1,31 @@
 package br.com.spock.service;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import com.tngtech.jgiven.junit5.SimpleScenarioTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import com.tngtech.jgiven.junit5.ScenarioTest;
+import br.com.spock.stages.ProdutoGivenStage;
+import br.com.spock.stages.ProdutoThenStage;
+import br.com.spock.stages.ProdutoWhenStage;
 
-public class ProdutoServiceTest extends SimpleScenarioTest<ProdutoServiceTest.Stages> {
-
-    public static class Stages {
-
-        ProdutoService service;
-        Double total;
-        List<String> nomes;
-
-        // GIVEN
-        public Stages given_umProdutto() {
-            service = new ProdutoService();
-            service.init();
-            return this;
-        }
-
-        // WHEN
-        public Stages when_eu_calculo_o_total() {
-            total = service.total();
-            return this;
-        }
-
-        public Stages when_eu_busco_os_nomes_filtrados() {
-            nomes = service.streamNomes();
-            return this;
-        }
-
-        // THEN
-        public Stages then_o_total_deve_ser(Double esperado) {
-            assert total.equals(esperado);
-            return this;
-        }
-
-        public Stages then_deve_retornar_dois_nomes() {
-            assert nomes.size() == 2;
-            return this;
-        }
-
-        public Stages deve_conter_nomes_corretos() {
-            assert nomes.contains("micro hp");
-            assert nomes.contains("micro del");
-            return this;
-        }
-    }
+public class ProdutoServiceTest extends ScenarioTest<ProdutoGivenStage, ProdutoWhenStage, ProdutoThenStage> {
 
     @Test
-    public void deve_calcular_total() {
-        given().given_umProdutto().when_eu_calculo_o_total().then_o_total_deve_ser(14500.0);
+    void deve_calcular_soma_total(){
+        given().um_catalago_padrao();
+        when().calculo_o_total();
+        then().o_total_deve_ser(14500.0);
     }
 
-    @Test
-    void teste_nomes() {
-        given().given_umProdutto()
-                .when_eu_busco_os_nomes_filtrados()
-                .then_deve_retornar_dois_nomes();
+    @ParameterizedTest(name = "faixa R${0}-R${1} retorna {2} produto(s)")
+    @CsvSource({
+        "4000.0, 6000.0, 3",
+        "4000.0, 4499.0, 1",
+        "0.0, 3999.0, 0" 
+    })
+    void deve_filtrar_por_faixa_de_preco(double precoMin, double precoMax, int qntProdutosEsperada){
+        given().um_catalago_padrao();
+        when().busco_por_produtos_faixa_preco(precoMin, precoMax);
+        then().a_quantidade_produtos_deve_ser(qntProdutosEsperada);
     }
 }
