f09617aa3d1474655d7a9bad7f406fc976071f55
danielavescia <danivescia@gmail.com>
2026-06-17 14:15:30 -0300

    refactor: change produto field to Optional<Produto> and update assertions in ProdutoThenStage

--- src/test/java/br/com/spock/stages/ProdutoThenStage.java
+++ src/test/java/br/com/spock/stages/ProdutoThenStage.java
@@ -1,6 +1,7 @@
 package br.com.spock.stages;
 
 import java.util.List;
+import java.util.Optional;
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import br.com.spock.model.Produto;
@@ -15,7 +16,7 @@ public class ProdutoThenStage extends Stage<ProdutoThenStage>{
     List<Produto> produtos;
 
     @ExpectedScenarioState
-    Produto produto;
+    Optional<Produto> produto;
 
     @ExpectedScenarioState
     List<String> nomes;
@@ -31,17 +32,21 @@ public class ProdutoThenStage extends Stage<ProdutoThenStage>{
     }
 
     public ProdutoThenStage produto_deve_ter_preco(Produto produtoEsperado){
-        assert produto.getPreco() == produtoEsperado.getPreco(): "Esperado: " + produtoEsperado.getPreco() + "produto obtido: " + produto.getPreco();
+        assert produto.isPresent();
+        Produto p = produto.get();
+        assert p.getPreco() == produtoEsperado.getPreco(): "Esperado: " + produtoEsperado.getPreco() + "produto obtido: " + p.getPreco();
         return self();
     }
 
     public ProdutoThenStage o_produto_deve_ter_nome(Produto produtoEsperado){
-        assert produto.getNomeProduto() == produtoEsperado.getNomeProduto(): "Nome Esperado: " + produtoEsperado.getNomeProduto() + "nome obtido: " + produto.getNomeProduto();
+        assert produto.isPresent();
+        Produto p = produto.get();
+        assert p.getNomeProduto().equals(produtoEsperado.getNomeProduto()): "Nome Esperado: " + produtoEsperado.getNomeProduto() + "nome obtido: " + p.getNomeProduto();
         return self();
     }
     public ProdutoThenStage o_produto_deve_ter_tipo(TypeProduto tipoEsperado){
         assert produtos.stream()
-            .allMatch(p -> p.getTipoProduto()  == tipoEsperado): "Tipo Esperado: " + tipoEsperado + "tipo obtido: " + produto.getTipoProduto();
+            .allMatch(p -> p.getTipoProduto()  == tipoEsperado): "Tipo Esperado: " + tipoEsperado;
         return self();
     }
 }
